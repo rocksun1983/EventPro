d7e0d95f512ed1e2773dca828cc2336a989cad85
ezek9292 <ezekdonkor@gmail.com>
2026-03-07 18:06:11 +0000

    minor changes to register route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,23 @@
 import express from "express";
-import { registerUser, loginUser, resetPasswordAuthenticated, forgotPassword, resetPasswordWithToken, updateProfile, getProfile } from "../controllers/authController.js";
+import {
+  registerUser,
+  loginUser,
+  resetPasswordAuthenticated,
+  forgotPassword,
+  resetPasswordWithToken,
+  updateProfile,
+  getProfile,
+  resendVerification,
+  verifyEmail
+} from "../controllers/authController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/register", registerUser);
+router.post("/signup", registerUser);
 router.post("/login", loginUser);
+router.post("/resend-verification", resendVerification);
+router.post("/verify-email", verifyEmail);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password/:token", resetPasswordWithToken);
 router.post("/reset-password", protect, resetPasswordAuthenticated);
@@ -13,4 +25,3 @@ router.get("/profile", protect, getProfile);
 router.put("/profile", protect, updateProfile);
 
 export default router;
-

823f4c33c88d0d5b8b8941b15b7f875adb05b5e9
ezekiel <ezekdonkor@gmail.com>
2026-03-16 20:24:35 +0000

    fixing the verification

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -25,7 +25,9 @@ const sendWithNodemailer = async ({ to, subject, text, html }) => {
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD
-    }
+    },
+    // Force IPv4 to avoid ENETUNREACH on IPv6-only SMTP resolution
+    family: 4
   });
 
   await transporter.sendMail({
